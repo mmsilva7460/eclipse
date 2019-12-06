399c310cc69d5b78c091f7b419169528f964921c
mmsilva7460 <58566938+mmsilva7460@users.noreply.github.com>
2019-12-05 21:18:04 -0300

    adicionei arquivos via uploads

---
+++ HelloGitRub/src/br/com/maga/git/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.maga.git;
+
+/**
+ * @author help
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
